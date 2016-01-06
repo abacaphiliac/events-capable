87ae103600adec1fa21038b1212e7b1f899cb783
Tim Younger <tim@webpt.com>
2016-01-05 18:33:36 -0700

    abstract-plugin-manager - allow this initializer to be added to any plugin-manager config. registered listener services must be provided by service_manager.

--- tests/EventsCapable/EventsCapableInitializerTest.php
+++ tests/EventsCapable/EventsCapableInitializerTest.php
@@ -6,7 +6,9 @@ use abacaphiliac\EventsCapable\EventsCapableInitializer;
 use Zend\EventManager\EventManager;
 use Zend\EventManager\EventsCapableInterface;
 use Zend\EventManager\ListenerAggregateInterface;
+use Zend\Hydrator\HydratorPluginManager;
 use Zend\ServiceManager\ServiceManager;
+use Zend\Validator\ValidatorPluginManager;
 
 class EventsCapableInitializerTest extends \PHPUnit_Framework_TestCase
 {
@@ -62,5 +64,33 @@ class EventsCapableInitializerTest extends \PHPUnit_Framework_TestCase
         $actual = $this->sut->initialize(new \stdClass(), new ServiceManager());
 
         $this->assertNull($actual);
+    }
+
+    public function testInitializeFromPluginManager()
+    {
+        $events = new EventManager();
+
+        $instance = $this->getMock('\Zend\EventManager\EventsCapableInterface');
+        $instance->method('getEventManager')->willReturn($events);
+
+        $listener = $this->getMock('\Zend\EventManager\ListenerAggregateInterface');
+        $listener->expects($this->once())->method('attach')->with($events);
+
+        $services = new ServiceManager();
+        $services->setService('MyListener', $listener);
+        $services->setService('config', array(
+            'abacaphiliac/events-capable' => array(
+                'eventsCapable' => array(
+                    get_class($instance) => array(
+                        'MyListener',
+                    ),
+                ),
+            ),
+        ));
+        
+        $validators = new ValidatorPluginManager();
+        $validators->setServiceLocator($services);
+
+        $this->sut->initialize($instance, $validators);
     }
 }
